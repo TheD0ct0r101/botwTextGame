81243d0322100077c1bc79378894880714bcf45f
TheD0ct0r101 <scpgovernment@gmail.com>
2020-09-11 21:03:12 -0700

    Attempting to add major Great Plateau Overworld locations

--- World.py
+++ World.py
@@ -1,3 +1,7 @@
+
+import cmd
+import textwrap
+
 DESC = 'desc'
 NORTH = 'north'
 SOUTH = 'south'
@@ -14,35 +18,72 @@ TAKEABLE = 'takeable'
 EDIBLE = 'edible'
 DESCWORDS = 'descwords'
 
+
 SCREEN_WIDTH = 80
 
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-def over_world_location(coordinate):
-    over_world_location_name = {
+# Converting xy coordinate values to location names via switch case \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+def overWorldLocation(coordinate):
+    overWorldLocationName = {
+        -5.1: 'River of the Dead',
+        -4.1: 'River of the Dead',
         -4.0: 'Keh Namut Shrine',
+        -3.4: 'Forest of Spirits',
+        -3.3: 'Hopper Pond',
+        -2.4: 'Forest of Spirits',
+        -2.3: 'Forest of Spirits',
+        -1.4: 'Forest of Spirits',
+        -1.3: 'Forest of Spirits',
+        -1.2: 'Forest of Spirits',
         0.0: 'Shrine of Resurrection',
+        0.2: 'Forest of Spirits',
+        0.3: 'Forest of Spirits',
+        0.4: 'Forest of Spirits',
         1.-4: 'Owa Daim Shrine',
-        1.0: '1,0',
+        1.3: 'Forest of Spirits',
         2.0: 'Temple of Time',
-        3.0: '3,0',
+        3.-2: 'Mount Hylia',
         3.4: 'Oman Au Shrine',
         4.2: 'Great Plateau Tower',
         6.0: 'Ja Baij Shrine',
     }
-    return over_world_location_name.get(coordinate, "Location does not exist")
+    return overWorldLocationName.get(coordinate, "Location does not exist")
 
+def overWorldLocationRoom(roomIdValue):
+    overWorldLocationRoomName = {}
+    return overWorldLocationRoomName.get(roomIdValue, "Room does not exist")
+
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+# List of World locations and their properties \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
 worldLocations = {
-    over_world_location(0.0): {
-     DESC: "Your place of awakening.",
-     GROUND: ['Sheikah Slate']
+    overWorldLocation(0.0): {
+        overWorldLocationRoom(0): {
+            DESC: "This chamber holds the stone tub from which you awoke. It was once filled with a mysterious blue "
+                  "liquid",
+            GROUND: ['Sheikah Slate'],
+        },
+        DESC: "Your place of awakening.",
     }
 }
 
 
-def displayLocation(loc):
-    print(loc)
-    print('=' * len(loc))
+# //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+# Displaying/accessing area properties \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-displayLocation(over_world_location(0.0))
+# displayLocation(overWorldLocation(coordinate)
+
+def displayLocation(coordinate):
+
+    # Print location name with fancy divider
+    print(coordinate)
+    print('=' * len(coordinate))
+
+    # Print location description with textwrap.wrap()
+    print('\n'.join(textwrap.wrap(worldLocations[coordinate][DESC], SCREEN_WIDTH)))
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
